b9f3ccf099cbb9ead5623035b16629c4fe1c8a93
cLinares24 <108821790+cLinares24@users.noreply.github.com>
2023-05-02 11:02:08 -0500

    Delete CRUD directory

--- CRUD/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-//datos de configuracion MYSQL
-define ('host', 'localhost');
-define('user', 'root');
-define('password', 'root');
-define('database', 'dbpoophp');
-
--- CRUD/student/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("../class/student.php");
-
-$stu = new Student();
-$code = $_GET['cod'];
-$stu->deleteStudent($code);
